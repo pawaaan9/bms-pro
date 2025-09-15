13a6f0801cd3d8d63323677e71df429f33feda3d
pawaaan9 <pawankanchana99@gmail.com>
2025-09-15 09:47:46 +0530

    booking

--- src/components/ui/toaster.jsx
+++ src/components/ui/toaster.jsx
@@ -1,33 +1,6 @@
-import { useToast } from "@/components/ui/use-toast";
-import {
-  Toast,
-  ToastClose,
-  ToastDescription,
-  ToastProvider,
-  ToastTitle,
-  ToastViewport,
-} from "@/components/ui/toast";
-
+// Simple toaster component that doesn't require external dependencies
 export function Toaster() {
-  const { toasts } = useToast();
-
-  return (
-    <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
-        return (
-          <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && (
-                <ToastDescription>{description}</ToastDescription>
-              )}
-            </div>
-            {action}
-            <ToastClose />
-          </Toast>
-        );
-      })}
-      <ToastViewport />
-    </ToastProvider>
-  );
+  // This is a placeholder toaster component
+  // The actual toast functionality is handled by ToastNotification component
+  return null;
 } 
